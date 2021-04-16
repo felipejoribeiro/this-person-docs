4b227d0c0e66d388e4cd6a626c794df4991fd649
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-04-16 18:23:45 -0300

    programming update

--- programming/backend/node_js/server_2.js
+++ programming/backend/node_js/server_2.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Middle ware
+app.use((req,res,next) => {
+	next();
+})
+
 app.get('/', (req, res) => {
 	res.send('<h1> hello </h1>');
+})
+
+app.get('/profile/:id', (req,res) => {
+
+  console.log(req.query);
+	console.log(req.body);
+  console.log(req.params);
+  console.log(req.headers);
+
+	res.send('success');
 })
 
 app.listen(2033);
